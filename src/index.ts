c732cd1ca072c34f5845a8601a05cf634442b077
Steve Haslam <araqnid@gmail.com>
2022-03-28 15:59:59 +0100

    Use aws-sdk implementation to read config file

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,18 @@
 import fs from "fs"
-import {readlines} from "readlines"
 import {GetRoleCredentialsCommand, SSOClient} from "@aws-sdk/client-sso";
+import {loadSharedConfigFiles} from "@aws-sdk/shared-ini-file-loader";
 
 if (process.argv.length < 3) {
   console.error("Specify profile name as parameter")
   process.exit(1)
 }
 
-async function loadProfileSSOConfig(profile: string): Promise<Record<string, string>> {
-  const configFile = `${process.env["HOME"]}/.aws/config`
-  const lines = await new Promise<string[]>(((resolve, reject) => {
-    readlines(configFile, (err: any, lines: string[]) => {
-      if (err) reject(err)
-      else resolve(lines)
-    })
-  }))
-  let section: string = "default"
-  const settings: Record<string, string> = {}
-  for (const line of lines) {
-    const sectionMatch = line.match(/^\[(.+)]$/)
-    if (sectionMatch) {
-      section = sectionMatch[1]
-      continue
-    }
-    const settingMatch = line.match(/^([^ ]+) *= *(.+)/)
-    if (settingMatch && section === `profile ${profile}`) {
-      settings[settingMatch[1]] = settingMatch[2]
-    }
-  }
-  if (Object.keys(settings).length === 0) {
-    throw new Error(`Profile "${profile}" not found in ${configFile}`)
-  }
-  return settings
-}
-
 async function main(profile: string) {
   const credentialsCacheFile = `${process.env["HOME"]}/.aws/sso/cache/6cd2b2dcd05b0cd585381193b0b81dbf3e62d5b2.json`
   const {accessToken, region} = JSON.parse(await fs.promises.readFile(credentialsCacheFile, "utf-8"))
 
-  const ssoConfig = await loadProfileSSOConfig(profile)
+  const sharedConfig = await loadSharedConfigFiles()
+  const ssoConfig = sharedConfig.configFile[profile]
   const ssoClient = new SSOClient({region})
   try {
     const response = await ssoClient.send(new GetRoleCredentialsCommand({
