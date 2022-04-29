09dd6cb706d8d21f1268f5c451a499d5b56567c7
Steve Haslam <steve.haslam@iongroup.com>
2022-04-29 12:34:59 +0100

    Handle 401 errors from SSO client and do 'sso login'

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,75 @@
 import fs from "fs"
-import {GetRoleCredentialsCommand, SSOClient} from "@aws-sdk/client-sso";
+import {GetRoleCredentialsCommand, GetRoleCredentialsCommandOutput, SSOClient} from "@aws-sdk/client-sso";
 import {loadSharedConfigFiles} from "@aws-sdk/shared-ini-file-loader";
+import {Profile} from "@aws-sdk/types";
+import {spawn} from "child_process";
+
+async function loadAccessToken(): Promise<string> {
+  const credentialsCacheFile = `${process.env["HOME"]}/.aws/sso/cache/6cd2b2dcd05b0cd585381193b0b81dbf3e62d5b2.json`
+  const {accessToken} = JSON.parse(await fs.promises.readFile(credentialsCacheFile, "utf-8"))
+  return accessToken
+}
+
+async function attemptSSOLogin(profileName: string) {
+  return new Promise((resolve, reject) => {
+    const command = spawn("aws", ["--profile", profileName, "sso", "login"], {
+      stdio: ["inherit", "pipe", "pipe"]
+    })
+
+    command.stdout.setEncoding("utf-8")
+    command.stdout.on("data", data => {
+      for (const line of data.split('\n')) {
+        console.log(`aws sso login: ${line}`)
+      }
+    })
+
+    command.stderr.setEncoding("utf-8")
+    command.stderr.on("data", data => {
+      for (const line of data.split('\n')) {
+        console.log(`aws sso login:(stderr): ${line}`)
+      }
+    })
+
+    command.on("error", err => {
+      reject(err)
+    })
+
+    command.on("close", exitCode => {
+      if (exitCode === 0)
+        resolve(undefined)
+      else
+        reject(new Error(`aws command terminated with exit code ${exitCode}`))
+    })
+  })
+}
+
+async function getRoleCredentialsPossiblyLogin(ssoClient: SSOClient, ssoConfig: Profile, profileName: string): Promise<GetRoleCredentialsCommandOutput> {
+  try {
+    return await ssoClient.send(new GetRoleCredentialsCommand({
+      accountId: ssoConfig.sso_account_id,
+      roleName: ssoConfig.sso_role_name,
+      accessToken: await loadAccessToken()
+    }));
+  } catch (err: any) {
+    if (err["$fault"] === "client" && err["$metadata"].httpStatusCode === 401) {
+      await attemptSSOLogin(profileName)
+      return await ssoClient.send(new GetRoleCredentialsCommand({
+        accountId: ssoConfig.sso_account_id,
+        roleName: ssoConfig.sso_role_name,
+        accessToken: await loadAccessToken()
+      }));
+    }
+    throw err
+  }
+}
 
 async function main(profile?: string) {
   const sharedConfig = await loadSharedConfigFiles()
   if (profile) {
-    const credentialsCacheFile = `${process.env["HOME"]}/.aws/sso/cache/6cd2b2dcd05b0cd585381193b0b81dbf3e62d5b2.json`
-    const {accessToken} = JSON.parse(await fs.promises.readFile(credentialsCacheFile, "utf-8"))
-
     const ssoConfig = sharedConfig.configFile[profile]
     const ssoClient = new SSOClient({region: ssoConfig.sso_region})
     try {
-      const response = await ssoClient.send(new GetRoleCredentialsCommand({
-        accountId: ssoConfig.sso_account_id,
-        roleName: ssoConfig.sso_role_name,
-        accessToken
-      }))
+      const response = await getRoleCredentialsPossiblyLogin(ssoClient, ssoConfig, profile)
 
       if (response.roleCredentials) {
         const {accessKeyId, secretAccessKey, sessionToken} = response.roleCredentials
