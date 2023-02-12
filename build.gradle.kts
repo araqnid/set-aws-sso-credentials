63f5788eb3b8a48321f22322b551352465118360
Steve Haslam <araqnid@gmail.com>
2023-02-12 17:01:27 +0000

    Upgrade AWS-SDK packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,8 +30,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json")
     implementation(platform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:$kotlinWrappers"))
     implementation("org.jetbrains.kotlin-wrappers:kotlin-node")
-    implementation(npm("@aws-sdk/client-sso", "^3.145.0"))
-    implementation(npm("@aws-sdk/shared-ini-file-loader", "^3.127.0"))
+    implementation(npm("@aws-sdk/client-sso", "^3.267.0"))
+    implementation(npm("@aws-sdk/shared-ini-file-loader", "^3.267.0"))
 
     testImplementation(kotlin("test-js"))
     testImplementation("org.araqnid.kotlin.assert-that:assert-that:0.1.1")
