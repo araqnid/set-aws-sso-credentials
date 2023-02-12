6c1913c31e06fab6394021a6989ef3a37715b3f5
Steve Haslam <araqnid@gmail.com>
2023-02-12 19:30:06 +0000

    Use indexOf to avoid looping in splitLines

---
+++ src/test/kotlin/org/araqnid/kotlin/setawsssocredentials/CommandTest.kt
@@ -0,0 +1,28 @@
+@file:OptIn(ExperimentalCoroutinesApi::class)
+
+package org.araqnid.kotlin.setawsssocredentials
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.test.runTest
+import org.araqnid.kotlin.assertthat.assertThat
+import org.araqnid.kotlin.assertthat.equalTo
+import kotlin.test.Test
+
+class CommandTest {
+    @Test
+    fun run_command() = runTest {
+        val lines = flowOf("\nabc\ndef\n", "gh", "i\n").splitLines().toList()
+        assertThat(
+            lines, equalTo(
+                listOf(
+                    "",
+                    "abc",
+                    "def",
+                    "ghi"
+                )
+            )
+        )
+    }
+}
