e7223b94a591d46e66a8500dde99155cf05dd2cb
Steve Haslam <araqnid@gmail.com>
2023-03-06 00:18:00 +0000

    Output error if process exits without top-level Job completing

--- src/main/kotlin/org/araqnid/kotlin/setawsssocredentials/RunScript.kt
+++ src/main/kotlin/org/araqnid/kotlin/setawsssocredentials/RunScript.kt
@@ -1,8 +1,11 @@
 package org.araqnid.kotlin.setawsssocredentials
 
+import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import node.events.Event
+import node.os.EOL
 import node.process.process
 import kotlin.coroutines.Continuation
 import kotlin.coroutines.CoroutineContext
@@ -12,6 +15,33 @@ import kotlin.coroutines.startCoroutine
 fun runScript(context: CoroutineContext = EmptyCoroutineContext, body: suspend CoroutineScope.() -> Unit) {
     val job = Job(parent = context[Job])
     val scope = CoroutineScope(Dispatchers.Default + context + job)
+
+    process.on(Event.EXIT) {
+        if (job.isCompleted) return@on
+
+        console.error(buildString {
+            fun appendJob(cursor: Job, level: Int) {
+                for (i in 0 until level) {
+                    append("-> ")
+                }
+                append(cursor)
+                if (cursor is CoroutineScope) {
+                    val coroutineName = cursor.coroutineContext[CoroutineName]
+                    if (coroutineName != null) {
+                        append(": ")
+                        append(coroutineName.name)
+                    }
+                }
+                append(EOL)
+                for (child in cursor.children) {
+                    appendJob(child, level + 1)
+                }
+            }
+
+            append("process exiting without script job terminating: $job$EOL")
+            appendJob(job, 0)
+        })
+    }
 
     job.invokeOnCompletion { ex ->
         if (ex != null) {
